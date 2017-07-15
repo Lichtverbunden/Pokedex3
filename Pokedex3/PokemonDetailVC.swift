710bd11b92e370b56c2db56a7cb4f57a3541c4f3
Ken Krippeler <kriken19@gmail.com>
2017-07-15 20:29:36 +0200

    Finished Layout and added Alamofire

--- Pokedex3/PokemonDetailVC.swift
+++ Pokedex3/PokemonDetailVC.swift
@@ -13,6 +13,17 @@ class PokemonDetailVC: UIViewController
     var pokemon: Pokemon!
 
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var defenseLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var pokedexLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var baseAttackLabel: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLabel: UILabel!
     
     override func viewDidLoad()
     {
@@ -21,6 +32,12 @@ class PokemonDetailVC: UIViewController
         nameLabel.text = pokemon.name.capitalized
 
     }
+    
+    @IBAction func backButtonPressed(_ sender: UIButton)
+    {
+        dismiss(animated: true, completion: nil)
+    }
+    
 
   
 
